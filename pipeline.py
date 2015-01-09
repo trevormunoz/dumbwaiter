51689d1ffdec4eeb6319d40b924acae525a5a1eb
Trevor Muñoz <trevor.munoz@gmail.com>
2015-01-09 18:16:23 -0500

    Clean up and reorganize by hand

--- pipeline.py
+++ pipeline.py
@@ -1,120 +1,47 @@
 
 # coding: utf-8
 
-# # A Repeatable Extract-Tranform-Load Pipeline for NYPL Menus Data
-# 
-# **Created:** 17 October 2014
-# 
-# **Updated:** 28 October 2014, 19 November 2014, 3 December 2014, 14 December 2014, 8 January 2015
-# 
-# **Authors:** Trevor Muñoz and Katie Rawson
-# 
-# &nbsp;
+# A Repeatable Extract-Tranform-Load Pipeline for NYPL Menus Data
 
-# Have you set environment variables?: 
-# 
-# * MENUS_LOG_HOME
-# * MENUS_SOURCE_DATA
-# * MENUS_OUTPUT_DIR
-# * MENUS_ES_HOSTNAME, 
-# * MENUS_ES_HOST_PORT
+# authors: Trevor Muñoz and Katie Rawson
+# created: 17 October 2014
+# updated: 28 October 2014, 19 November 2014, 3 December 2014,
+#     14 December 2014, 8-9 January 2015
 
-# ### Acquiring Data (Extract)
-
-# At the moment, the anti-bot protections on the New York Public Library's Web site make it impossible to grab the latest data download link directly. So, we download the data (as gzipped tar file) and package it with this script. 
-# 
-# The first stage of the pipeline unzips and untars the archive into four CSV files:
-# 
-# * Dish.csv
-# * Menu.csv
-# * MenuItem.csv
-# * MenuPage.csv
-# 
-# &nbsp;
-
-# In[ ]:
-
-import os
+from collections import Counter
 import datetime
-import time
-import tarfile
-
-
-# Set up logging …
-
-# In[ ]:
-
+import elasticsearch
+from elasticsearch import helpers
+import json
 import logging
 import logging.handlers
+import os
+import pandas as pd
+import pytz
+import re
+import tarfile
+import time
 
-
-# In[ ]:
-
-LOG_FILENAME = os.path.join(os.environ['MENUS_LOG_HOME'], 'nypl_menus_data_transform.log')
+# Set up logging
+LOG_FILENAME = os.path.join(os.environ['MENUS_LOG_HOME'],
+                            'nypl_menus_data_transform.log')
 
 pipeline_logger = logging.getLogger('MenusDataTransformLogger')
 pipeline_logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=500000, backupCount=10)
+handler = logging.handlers.RotatingFileHandler(
+    LOG_FILENAME,
+    maxBytes=500000,
+    backupCount=10)
 handler.setFormatter(formatter)
 pipeline_logger.addHandler(handler)
 
+UTC = pytz.utc
+os.environ['MENUS_ES_HOSTNAME'] = 'localhost'
+os.environ['MENUS_ES_HOST_PORT'] = '5000'
 
-# In[ ]:
-
-pipeline_logger.info('Menus ETL Pipeline: Starting run …')
-
-
-# In[ ]:
-
-srcfile = [x for x in os.listdir(os.environ['MENUS_SOURCE_DATA']) if os.path.splitext(x)[1] == '.tgz'][0]
-
-
-# In[ ]:
-
-tar = tarfile.open(os.path.join(os.environ['MENUS_SOURCE_DATA'], srcfile))
-
-
-# In[ ]:
-
-pipeline_logger.info('Extracting source files …')
-pipeline_logger.info('Listing contents of the tar package …')
-for tf in tar.getmembers():
-    pipeline_logger.info('Name: {0} \t Last Modified: {1}'.format(tf.name, time.ctime(tf.mtime)))
-
-
-# In[ ]:
-
-pipeline_logger.info('Untarring and unzipping …')
-tar.extractall(path=os.environ['MENUS_SOURCE_DATA'])
-
-for f in os.listdir(os.environ['MENUS_SOURCE_DATA']):
-    if f.endswith('csv'):
-        if os.path.isfile(os.path.join(os.environ['MENUS_SOURCE_DATA'], f)) == True:
-            pipeline_logger.info('{0} … \u2713'.format(f))
-
-
-# In[ ]:
-
-tar.close()
-
-
-# ### Working with Data in DataFrames (Tranform)
-# 
-# &nbsp;
-
-# In[ ]:
-
-import re
-import json
-import pytz
-import pandas as pd
-
-
-# #### Helper Functions
-
-# In[ ]:
 
 def normalize_names(obj):
     '''
@@ -131,8 +58,6 @@ def normalize_names(obj):
     return result
 
 
-# In[ ]:
-
 def fingerprint(obj):
     """
     A modified version of the fingerprint clustering algorithm implemented by Open Refine.
@@ -148,28 +73,24 @@ def fingerprint(obj):
     return fingerprint
 
 
-# In[ ]:
-
 # TOFIX: This code is very slow
-utc = pytz.utc
-
 def reformat_dates(obj):
     naive_date_in = datetime.datetime.strptime(obj, '%Y-%m-%d %H:%M:%S %Z')
-    date_in = naive_date_in.replace(tzinfo=utc)
+    date_in = naive_date_in.replace(tzinfo=UTC)
     date_out = date_in.strftime('%Y%m%dT%H%M%S%z')
     return date_out
 
 
-# In[ ]:
-
 def reshape_data(obj):
     '''
-    Takes JSON, loads it, and spits out a dictionary of slightly different shape
-    needed for bulk import by Elasticsearch.
+    Takes JSON, loads it, and spits out a dictionary of slightly different
+    shape needed for bulk import by Elasticsearch.
     
-    Input is JSON rather than simply another dictionary so I can punt on properly
-    serializing things like 'NaN' by leaving that to pandas own to_json() function.
+    Input is JSON rather than simply another dictionary so I can punt on
+    properly serializing things like 'NaN' by leaving that to pandas own
+    to_json() function.
     '''
+
     data = json.loads(obj)
     action = {
               "_index": "menus",
@@ -180,25 +101,149 @@ def reshape_data(obj):
     return action
 
 
-# #### DataFrame Loading
-
-# In[ ]:
-
-from collections import OrderedDict
-
-def load_dataframes(path):
+def server(hostname=os.environ['MENUS_ES_HOSTNAME'], host_port=os.environ['MENUS_ES_HOST_PORT']):
     """
-    Takes a file path where CSV files to be processed are located
-    and returns a single merged pandas DataFrame with the relevant
-    data
+    Makes sure that an elasticsearch server is running.
+    Sets up the appropriate index and mapping.
     """
+    
+    pipeline_logger.info('Verifying Elasticsearch server is ready to receive data …')
+    es = elasticsearch.Elasticsearch([{'host': hostname, 'port': host_port}])
+
+    index_name = 'menus'
+    type_name = 'item'
+
+    # Check if the index already exists and, if so, delete it to keep things idempotent
+    if es.indices.exists(index_name):
+        pipeline_logger.info("deleting '{0}' index...".format(index_name))
+        res = es.indices.delete(index = index_name)
+        pipeline_logger.info(" response: '{0}'".format(res))
+
+    pipeline_logger.info("creating '{0}' index...".format(index_name))
+    res = es.indices.create(index = index_name)
+    pipeline_logger.info(" response: '{0}'".format(res))
+
+    item_mapping = {"properties": {
+        'item_updated_at': {
+            "type": "date",
+            "format": "basic_date_time_no_millis"
+        },
+        'menu_page_id': {
+            "type": "long",
+        } ,
+     'menu_sponsor': {
+        "type": "string",
+        "index": "not_analyzed"
+        },
+     'menu_page_count': {
+        "type": "double"
+        },
+     'item_ypos': {
+        "type": "double"
+        },
+     'dish_normalized_name': {
+        "type": "string"
+        },
+     'menu_id': {
+            "type": "long",
+        },
+     'dish_times_appeared': {
+        "type": "double"
+        },
+     'menu_location': {
+        "type": "string",
+        "index": "not_analyzed"
+        },
+     'dish_menus_appeared': {
+        "type": "double"
+        },
+     'menu_uri': {
+        "type": "string",
+        "index": "not_analyzed"
+        },
+     'menu_page_uri': {
+        "type": "string",
+        "index": "not_analyzed"
+        },
+     'page_image_uuid': {
+        "type": "string"
+        },
+     'menu_page_number': {
+        "type": "double"
+        },
+     'item_created_at': {
+        "type": "date",
+        "format": "basic_date_time_no_millis"
+        },
+     'dish_id': {
+        "type": "long",
+        },
+     'dish_name': {
+        "type": "string",
+        "index": "not_analyzed"
+        },
+     'item_uri': {
+        "type": "string",
+        "index": "not_analyzed"
+        },
+     'item_id': {
+        "type": "long",
+        },
+     'image_id': {
+        "type": "long",
+        },
+     'item_xpos': {
+        "type": "double"
+        },
+     'dish_name_fingerprint': {
+        "type": "string",
+        "index": "not_analyzed"
+        },
+     'dish_uri': {
+        "type": "string",
+        "index": "not_analyzed"
+        }
+    }
+    }
+
+    pipeline_logger.info("Creating mapping for '{0}' index...".format(index_name))
+    res = es.indices.put_mapping(index='menus', doc_type='item', body=item_mapping)
+    pipeline_logger.info(" response: '{0}'".format(res))
+
+    return es
+
+
+def load_and_tranform(fpath):
+    """
+    Takes a file path where data file(s) to be processed is/are located
+    and returns a generator that yield dictionaries representing
+    documents to be indexed
+    """
+
+    srcfile = [x for x in os.listdir(fpath) if os.path.splitext(x)[1] == '.tgz'][0]
+    tar = tarfile.open(os.path.join(fpath, srcfile))
+
+    pipeline_logger.info('Extracting source files …')
+    pipeline_logger.info('Listing contents of the tar package …')
+    for tf in tar.getmembers():
+        pipeline_logger.info('Name: {0} \t Last Modified: {1}'.format(tf.name, time.ctime(tf.mtime)))
+
+    pipeline_logger.info('Untarring and unzipping …')
+    tar.extractall(path=fpath)
+
+    for f in os.listdir(fpath):
+        if f.endswith('csv'):
+            if os.path.isfile(os.path.join(fpath, f)) == True:
+                pipeline_logger.info('{0} … \u2713'.format(f))
+
+    tar.close()
     
     pipeline_logger.info('Loading data from source files into memory …')
 
-    LATEST_DISH_DATA_DF = pd.DataFrame.from_csv(os.path.join(path, 'Dish.csv'), index_col='id')
-    LATEST_ITEM_DATA_DF = pd.DataFrame.from_csv(os.path.join(path, 'MenuItem.csv'), index_col='dish_id')
-    LATEST_PAGE_DATA_DF = pd.DataFrame.from_csv(os.path.join(path, 'MenuPage.csv'), index_col='id')
-    LATEST_MENU_DATA_DF = pd.DataFrame.from_csv(os.path.join(path, 'Menu.csv'), index_col='id')
+    LATEST_DISH_DATA_DF = pd.DataFrame.from_csv(os.path.join(fpath, 'Dish.csv'), index_col='id')
+    LATEST_ITEM_DATA_DF = pd.DataFrame.from_csv(os.path.join(fpath, 'MenuItem.csv'), index_col='dish_id')
+    LATEST_PAGE_DATA_DF = pd.DataFrame.from_csv(os.path.join(fpath, 'MenuPage.csv'), index_col='id')
+    LATEST_MENU_DATA_DF = pd.DataFrame.from_csv(os.path.join(fpath, 'Menu.csv'), index_col='id')
 
     pipeline_logger.info('Data loaded. Starting transformations …')
     
@@ -207,7 +252,8 @@ def load_dataframes(path):
     #  Dish.csv
     #
     # =================================
-    
+
+    pipeline_logger.info('Working on Dish.csv …')
     NULL_APPEARANCES = LATEST_DISH_DATA_DF[LATEST_DISH_DATA_DF.times_appeared == 0]
     pipeline_logger.info('Data set contains {0} dishes that appear 0 times …'.format(len(NULL_APPEARANCES)))
     
@@ -240,6 +286,7 @@ def load_dataframes(path):
     #
     # =================================
     
+    pipeline_logger.info('Working on MenuItem.csv …')
     pipeline_logger.info('Reformatting item dates …')
     LATEST_ITEM_DATA_DF['item_created_at'] = LATEST_ITEM_DATA_DF.created_at.map(reformat_dates)
     LATEST_ITEM_DATA_DF['item_updated_at'] = LATEST_ITEM_DATA_DF.updated_at.map(reformat_dates)
@@ -261,7 +308,7 @@ def load_dataframes(path):
     #
     # =================================
     
-    #LATEST_PAGE_DATA_DF.head()
+    pipeline_logger.info('Working on MenuPage.csv …')
     LATEST_PAGE_DATA_DF[['full_height', 'full_width']].astype(int, raise_on_error=False)
     
     # =================================
@@ -270,8 +317,7 @@ def load_dataframes(path):
     #
     # =================================
     
-    LATEST_MENU_DATA_DF.columns
-    
+    pipeline_logger.info('Working on Menu.csv …')
     discarded_columns3 = [n for n in LATEST_MENU_DATA_DF.columns if n not in 
                       ['sponsor', 'location', 'date', 'page_count', 'dish_count']]
     pipeline_logger.info('Discarding columns from Menu.csv …')
@@ -334,213 +380,25 @@ def load_dataframes(path):
     return (reshape_data(row.to_json()) for i, row in FOR_JSON_OUTPUT.iterrows())
 
 
-# ### Load (to Elasticsearch)
+if __name__ == '__main__':
 
-# In[ ]:
+    pipeline_logger.info('Menus ETL Pipeline: Starting run …')
+    c = Counter()
 
-import elasticsearch
+    try:
+        client = server()
+        actioner = load_and_tranform(os.environ['MENUS_SOURCE_DATA'])
 
-
-# In[ ]:
-
-os.environ['MENUS_ES_HOSTNAME'] = 'localhost'
-os.environ['MENUS_ES_HOST_PORT'] = '5000'
-
-
-# In[ ]:
-
-pipeline_logger.info('Verifying Elasticsearch server is ready to receive data …')
-es = elasticsearch.Elasticsearch([{'host': os.environ['MENUS_ES_HOSTNAME'], 'port': os.environ['MENUS_ES_HOST_PORT']}])
-
-
-# In[ ]:
-
-INDEX_NAME = 'menus'
-TYPE_NAME = 'item'
-
-
-# Check if the index already exists and, if so, delete it to keep things idempotent
-
-# In[ ]:
-
-if es.indices.exists(INDEX_NAME):
-    pipeline_logger.info("deleting '{0}' index...".format(INDEX_NAME))
-    res = es.indices.delete(index = INDEX_NAME)
-    pipeline_logger.info(" response: '{0}'".format(res))
-
-
-# In[ ]:
-
-pipeline_logger.info("creating '{0}' index...".format(INDEX_NAME))
-res = es.indices.create(index = INDEX_NAME)
-pipeline_logger.info(" response: '{0}'".format(res))
-
-
-# In[ ]:
-
-item_mapping = {"properties": {
-    'item_updated_at': {
-        "type": "date",
-        "format": "basic_date_time_no_millis"
-    },
-    'menu_page_id': {
-        "type": "long",
-    } ,
- 'menu_sponsor': {
-    "type": "string",
-    "index": "not_analyzed"
-    },
- 'menu_page_count': {
-    "type": "double"
-    },
- 'item_ypos': {
-    "type": "double"
-    },
- 'dish_normalized_name': {
-    "type": "string"
-    },
- 'menu_id': {
-        "type": "long",
-    },
- 'dish_times_appeared': {
-    "type": "double"
-    },
- 'menu_location': {
-    "type": "string",
-    "index": "not_analyzed"
-    },
- 'dish_menus_appeared': {
-    "type": "double"
-    },
- 'menu_uri': {
-    "type": "string",
-    "index": "not_analyzed"
-    },
- 'menu_page_uri': {
-    "type": "string",
-    "index": "not_analyzed"
-    },
- 'page_image_uuid': {
-    "type": "string"
-    },
- 'menu_page_number': {
-    "type": "double"
-    },
- 'item_created_at': {
-    "type": "date",
-    "format": "basic_date_time_no_millis"
-    },
- 'dish_id': {
-    "type": "long",
-    },
- 'dish_name': {
-    "type": "string",
-    "index": "not_analyzed"
-    },
- 'item_uri': {
-    "type": "string",
-    "index": "not_analyzed"
-    },
- 'item_id': {
-    "type": "long",
-    },
- 'image_id': {
-    "type": "long",
-    },
- 'item_xpos': {
-    "type": "double"
-    },
- 'dish_name_fingerprint': {
-    "type": "string",
-    "index": "not_analyzed"
-    },
- 'dish_uri': {
-    "type": "string",
-    "index": "not_analyzed"
-    }
-}
-}
-
-
-# In[ ]:
-
-pipeline_logger.info("Creating mapping for '{0}' index...".format(INDEX_NAME))
-res = es.indices.put_mapping(index='menus', doc_type='item', body=item_mapping)
-pipeline_logger.info(" response: '{0}'".format(res))
-
-
-# In[ ]:
-
-# def chunk_actions(l, size):
-#     for i in range(0, len(l), size):
-#         yield l[i:i+size]
-
-
-# In[ ]:
-
-# from elasticsearch import helpers
-
-# pipeline_logger.info('Loading data into Elasticsearch …')
-
-# for batch in chunk_actions(DOCS_TO_INDEX, 100000):
-#     pipeline_logger.info(es.indices.stats(INDEX_NAME))
-#     helpers.bulk(es, batch)
-
-# pipeline_logger.info('Elasticsearch index ready!')
-
-
-# #### Reshaping DataFrame JSON to ES bulk upload JSON
-
-# In[ ]:
-
-# # From this SO answer: http://stackoverflow.com/questions/24527006/split-a-generator-into-chunks-without-pre-walking-it
-# from itertools import chain, islice
-
-# def chunks(iterable, size=10):
-#     iterator = iter(iterable)
-#     for first in iterator:
-#         yield chain([first], islice(iterator, size - 1))
-
-
-# In[ ]:
-
-from elasticsearch import helpers
-
-es_action_gen = load_dataframes(os.environ['MENUS_SOURCE_DATA'])
-
-pipeline_logger.info('Preparing data to load into Elasticsearch …')
-try:
-    for ok, result in helpers.streaming_bulk(es, es_action_gen, chunk_size=1000):
-        action, result = result.popitem()
-        doc_id = '/menus/item/{0}'.format(result['_id'])
-        if not ok:
-            pipeline_logger.error('Failed to {0} document {1}: {2}'.format(action, doc_id, result['error']))
-        else:
-            pipeline_logger.info(doc_id + 'succeeded')
-except BaseException as e:
-    pipeline_logger.error('Something went wrong: {0}'.format(str(e)))
-
-
-# In[ ]:
-
-# from elasticsearch import helpers
-
-# ROWGEN = load_dataframes(os.environ['MENUS_SOURCE_DATA'])
-
-# pipeline_logger.info('Preparing data to load into Elasticsearch …')
-# for batch in chunks(ROWGEN, size=100000):
-#     actions = [reshape_json(doc) for doc in batch]
-#     pipeline_logger.info('{0} actions ready for loading …'.format(len(actions)))
-#     #pipeline_logger.info(actions[0])
-#     res = helpers.bulk(es, actions)
-#     pipeline_logger.info(res)
-#     #pipeline_logger.info(es.indices.stats(INDEX_NAME))
-    
-# #pipeline_logger.info('Generator exhausted')
-# pipeline_logger.info('Elasticsearch index ready!')
-
-
-# In[ ]:
-
-
-
+        pipeline_logger.info('Preparing data to load into Elasticsearch …')
+        for ok, result in helpers.streaming_bulk(client, actioner, chunk_size=1000):
+            action, result = result.popitem()
+            doc_id = '/menus/item/{0}'.format(result['_id'])
+            if not ok:
+                pipeline_logger.error('Failed to {0} document {1}: {2}'.format(action, doc_id, result['error']))
+            else:
+                c[doc_id] += 1
+                
+            pipeline_logger.info('{0} action succeeded for {1} documents'.format(action, sum(c.values())))
+            
+    except BaseException as e:
+        pipeline_logger.error('Something went wrong: {0}'.format(str(e)))
